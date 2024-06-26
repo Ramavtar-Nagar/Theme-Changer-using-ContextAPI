5506b049f2a056c48140bd9388b1b7a471933be9
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-06-26 14:00:04 +0530

    Complete project fron adding components to app.js file, writing all the required functionalities and everything.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from "./components/ThemeBtn"
+import Card from "./components/Card"
 import './App.css'
 
 function App() {
@@ -14,18 +16,21 @@ function App() {
     setThemeMode("dark")
   }
 
-  
+  // functionality of actual change of theme
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
 
   return (
     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
     <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                {/* Theme button*/}
-                </div>
-
-                <div className="w-full max-w-sm mx-auto">
-                  {/* Card */}
+                  <ThemeBtn />
+              </div>
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
               </div>
           </div>
       </div>
